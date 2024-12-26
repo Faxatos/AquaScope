9af5140628849a63c9e5f9bb3b61309ad8b16659
Faxy <emilianosescu@gmail.com>
2024-12-26 18:29:36 +0100

    fixed skeleton page for alarms

--- OperatorUI/app/map/alarms/page.tsx
+++ OperatorUI/app/map/alarms/page.tsx
@@ -1,7 +1,7 @@
 import Pagination from '@/app/ui/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/alarms/table';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { AlarmsTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
 import { fetchTotalAlarmPages } from '@/app/lib/data'; 
@@ -25,7 +25,7 @@ export default async function Page(props: {
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search with MMSI..." />
       </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<AlarmsTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
