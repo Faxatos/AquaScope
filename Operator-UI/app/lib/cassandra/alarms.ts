98f83b93c8d171535dbadbb85784a23fecb5e76c
Faxatos <emilianosescu@gmail.com>
2025-02-04 16:16:45 +0100

    fixed fetchTotalAlarmPages method

--- Operator-UI/app/lib/cassandra/alarms.ts
+++ Operator-UI/app/lib/cassandra/alarms.ts
@@ -31,17 +31,28 @@ export const fetchAlarmPage = async (mmsi: string, currentPage: number): Promise
 // Fetch the total number of alarm pages based on the query
 export const fetchTotalAlarmPages = async (mmsi: string): Promise<number> => {
   try {
-    const response = await fetch(`/api/alarms?action=fetchTotalPages&mmsi=${mmsi}`);
+    // Remove the mmsi parameter from the URL if it's empty
+    const url = mmsi 
+      ? `/api/alarms?action=fetchTotalPages&mmsi=${mmsi}`
+      : `/api/alarms?action=fetchTotalPages`; // No MMSI in the URL
+      
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.error('Error fetching alarm data:', response.statusText);
+      return 1;
+    }
+
     const data = await response.json();
 
     if (data.error) {
       console.error('Error fetching total pages:', data.error);
-      return 0; // Default to 0 pages if there's an error
+      return 1; // Default to 1 pages if there's an error
     }
 
-    return data.data || 0; // Return the total number of pages
+    return data.data || 1; // Return the total number of pages
   } catch (error) {
     console.error('Error fetching total pages:', error);
-    return 0; // Default to 0 pages in case of network error
+    return 1; // Default to 1 pages in case of network error
   }
 };
