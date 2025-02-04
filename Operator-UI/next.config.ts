c7f46be0ddebda1419665ac98343cf60dcb82cd9
Faxatos <emilianosescu@gmail.com>
2025-02-04 11:15:08 +0100

    adding kerberos as external webpack config

--- Operator-UI/next.config.ts
+++ Operator-UI/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // Enabling React Strict Mode (recommended for development)
   reactStrictMode: true,
+
+  // Adding custom webpack configuration
+  webpack: (config) => {
+    // Add "kerberos" to the externals to prevent bundling it
+    config.externals = [...config.externals, "kerberos"];
+
+    // You can add more custom webpack configurations here if needed
+    return config;
+  },
+
+  // Set output to "standalone"
+  output: "standalone",
 };
 
 export default nextConfig;
